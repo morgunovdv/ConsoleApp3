bd9ef1a237ccc717e3058178f9c20177e896dd14
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-04-02 18:59:51 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Point
+    {
+        int _x = 80;
+        int _y = 40;
+
+        public int X => _x;
+        public int Y => _y;
+        //int X { get; }
+        //int Y { get; }
+        //public Point(int x, int y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
+
+        //public int Perimetr()
+        //{
+        //    int p = 2 * (X + Y);
+        //    Console.WriteLine(p);
+        //    return p;
+        //}
+
+        
+        
+
+    }
+}
---
+++ ConsoleApp3/Polygon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace ConsoleApp3
+{
+    class Polygon
+    {
+        
+        
+
+         
+        public int Poligon()
+        {
+            int p = 2 * (((X2 - X1) * 2) + ((Y2-Y1)*2));
+            return p;
+        }
+    }
+}
